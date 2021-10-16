5e624fcaadfb800985242f0f4667565ae28bcadb
edmondweiss <edmond.weiss@askapplications.com>
2021-10-16 14:26:13 -0400

    Add window object example

--- src/components/SomeComponent.svelte
+++ src/components/SomeComponent.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+
     let count: number = 0;
     let test = 'test';
+    let url;
 
     function incrementCount() {
         count += 1;
     }
+
+    onMount(() => {
+        console.log(window.location.href)
+        url = window.location.href
+    })
 </script>
 
 <style lang="scss">
@@ -16,6 +24,7 @@
 </style>
 
 <h2>Client Side JS Sample</h2>
+<h3>{url}</h3>
 <button on:click={incrementCount}>
   <span>Clicked</span> {count} {count === 1 ? 'time' : 'times'}
 </button>
